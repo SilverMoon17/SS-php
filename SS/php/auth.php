e93323b43feea678eab82ed3271bba908cfb0062
SilverMoon17 <50795723+SilverMoon17@users.noreply.github.com>
2021-03-20 20:50:45 +0200

    Add files via upload

---
+++ SS/php/auth.php
@@ -0,0 +1,31 @@
+<?php 
+    $login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
+    $pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
+
+    function alert($msg)
+    {
+        echo "<script type='text/javascript'>alert('$msg');</script>";
+    }
+
+
+    $pass = md5($pass."dfsfrtfdkoutbkvhcysbIITUR");
+
+    $mysql = new mysqli('ss', 'mysql', 'mysql', 'register-db');
+    
+    $result = $mysql -> query("SELECT * FROM `users` WHERE `login` = 
+    '$login' AND `pass` = '$pass'");
+
+    $user = $result->fetch_assoc();
+
+    if (count($user) == 0) {
+        alert("Пользователь не найден");
+        exit();
+    }
+
+    setcookie('username', $user['name'], time() + 60*60*45, "/");
+
+
+    $mysql->close();
+
+    header("Location: ../login.php");
+?>
